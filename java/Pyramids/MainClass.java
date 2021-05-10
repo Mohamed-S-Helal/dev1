10a98673b36d24b5bceaac69033f6878103563e6
unknown <mohamed.salah.pet@gmail.com>
2021-05-10 18:39:07 +0200

    make java folder

--- java/Pyramids/MainClass.java
+++
@@ -1,6 +0,0 @@
-class MainClass{
-    public static void main(String[] args) {
-        PyramidCsvDAO dao = new PyramidCsvDAO("pyramids.csv");
-        dao.getPyramids().forEach(System.out::println);
-    }
-}
